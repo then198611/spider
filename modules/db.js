48b5e0db0c1a6fc6acf6924aaa4345f71cdf9d7b
huangshuaihui <huangshuaihui>
2017-11-13 16:49:03 +0800

    settings.js

--- modules/db.js
+++ modules/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize');
+const S = require('../settings')
 
-const seq = new Sequelize('test','root','',{
-    host: '127.0.0.1',
+const seq = new Sequelize(S.SQL.DATABASE,S.SQL.USER,S.SQL.PASSWORD,{
+    host: S.SQL.HOST,
+    port: S.SQL.PORT,
     dialect: 'mysql',
     pool:{
         max: 5,
---
+++ settings.demo.js
@@ -0,0 +1,19 @@
+/**
+ * config
+ * sql
+ * redis
+ * port
+ * ...
+ */
+
+const S = {
+  SQL: {
+    DATABASE: 'ai_act',
+    USER: 'admin',
+    HOST: '127.0.0.1',
+    PORT: '3306',
+    PASSWORD: 'admin'
+  }
+};
+
+module.exports = S;
